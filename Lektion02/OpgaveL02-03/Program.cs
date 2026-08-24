e106d4a4c26254492d706679a075759280aa6063
Andreas <andreas0904@hotmail.com>
2026-08-24 12:10:52 +0200

    same

---
+++ Lektion02/OpgaveL02-03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OpgaveL02_03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Dette er et leetspeak eksempel";
+        Console.WriteLine(text.ToLeetSpeak());
+    }
+}
---
+++ OpgaveL02-03.cs
@@ -0,0 +1,6 @@
+namespace DefaultNamespace;
+
+public class OpgaveL02_03
+{
+    
+}
